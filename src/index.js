ee4407a36003f6d742606e8f534d2fe588f9a493
21598 <21598>
2023-12-12 13:09:50 +0200

    gg

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App"
 import './index.css';
+import firebase from 'firebase'
 
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyAJ3wjolp1QD4t5UupxRBS_qkOoSCqm9F8",
+    authDomain: "reac-f8ed1.firebaseapp.com",
+    projectId: "reac-f8ed1",
+    storageBucket: "reac-f8ed1.appspot.com",
+    messagingSenderId: "451001285109",
+    appId: "1:451001285109:web:8f10eb5a766111c59208bc",
+    measurementId: "G-84L06RKD0V"
+  });
 ReactDOM.render(<App />, document.getElementById('root'));
