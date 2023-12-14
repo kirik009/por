246dbe82eb028f24c6659f58ba3acbfa3c55944e
kirillboravik69 <kirillboravik69@gmail.com>
2023-12-14 15:00:13 +0300

    gg

--- src/useActions.js
+++
@@ -1,8 +0,0 @@
-import {useDispatch} from "react-redux"
-import { bindActionCreators } from "redux"
-import * as Actions from './redux/Actions/user.actions'
-
-export const useActions = () => {
-    const dispatch = useDispatch()
-    return bindActionCreators(Actions, dispatch)
-}
