10d906cb80bad25760014a601a8691f7f652178a
21598 <21598>
2023-12-19 09:33:01 +0200

    gg

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -7,51 +7,51 @@ import {Link, useNavigate, useParams } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { Context } from '../index.js';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, setDoc, getFirestore } from "firebase/firestore";
 
 
 const Users = () => {
     const dispatch = useDispatch()
     let  authen = useSelector((state) => state.userss.characters);
-    const {auth} = useContext(Context)
-    const {authhh, firestore} = useContext(Context)
+   // const {auth} = useContext(Context)
+    const {auth, firestore} = useContext(Context)
 const  removeCharacter =  (id) => {
     dispatch(actions.deleteUser(id))
-   console.log(id)
-   console.log(authen)
-    // firestore.collection("chars").where("id", "==", id+1).get()
-    // .then(querySnapshot => {
-    //     querySnapshot.docs[0].ref.delete();
-    // });
-//     firestore.collection('chars').get()
-//     .then(post => {
-//         post.filter((character, i) => { return i !== id;
-//         })
-// })
-// console.log(chars)
-}
+
+    firestore.collection("chars").where("id", "==", id+1).get()
+    .then(querySnapshot => {
+        querySnapshot.docs[0].ref.delete();
+    });
+    let i = 0
+    while(i < querySnapshot.docs.length){
+        
+        const docRef = doc(getFirestore(), firestore.collection("chars"))
+        setDoc(docRef, )
+        i++;
+    }
+   
 
 const handleSubmit = character => {
     dispatch(actions.addUser(character)) 
     firestore.collection('chars').add(character)
 }
 
-// useEffect(() => {
-//     let chars = []
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//     .then(res => {
-//         return res.json();
-//     })
-//     .then ((data) => {
-//         console.log(data[1]["name"]);
-//         for(let i = 0; i< data.length; i++)
-//         {
-//             //dispatch(actions.addUser({id:data[i]["id"],firstName: data[i]["name"],lastName: data[i]["username"],email: data[i]["email"] })) 
-//         chars.push({id:data[i]["id"],firstName: data[i]["name"],lastName: data[i]["username"],email: data[i]["email"] })
-//     }  
-//     }
-//     )
-// }, [])
+useEffect(() => {
+    //let chars = []
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => {
+        return res.json();
+    })
+    .then ((data) => {
+        console.log(data[1]["name"]);
+        for(let i = 0; i< data.length; i++)
+        {
+            dispatch(actions.addUser({id:data[i]["id"],firstName: data[i]["name"],lastName: data[i]["username"],email: data[i]["email"] })) 
+       // chars.push({id:data[i]["id"],firstName: data[i]["name"],lastName: data[i]["username"],email: data[i]["email"] })
+    }  
+    }
+    )
+}, [])
 const handleClick = () => {
     if(auth === undefined)
     dispatch(actions.exUser())
@@ -79,6 +79,6 @@ const handleClick = () => {
     </div>
     </div>
 );
-
+      }
 }
 export default Users
