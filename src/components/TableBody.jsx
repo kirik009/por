08932dfbeb9feaa067f36879e8655ae512381a39
21598 <21598>
2023-12-26 09:29:13 +0200

    gg

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -2,16 +2,22 @@ import React, { useState } from 'react';
 import '../components/flickity.css';
 import Flickity from 'react-flickity-component'
 import { useEffect } from 'react';
+import { Carousel } from 'react-responsive-carousel';
 
 const TableBody = props => {
+  
    // const rows = props.characterData.map((row, index) => {
         return (
+         // <div>
             <Flickity
             className='slider'
-            elementType='tr'
-            reloadOnUpdate
-         > {props.characterData.map((row, index) => {
+            elementType={'tr'}
+            
+         >
+             {/* <Carousel className='crsl'> */}
+         {props.characterData.map((row, index) => {
             return ( 
+               
                 <tr key={index} className='Plate'> 
                 <td>{row.id}</td>
                 <td>{row.firstName}</td>
@@ -20,10 +26,12 @@ const TableBody = props => {
                 <td><button onClick={() => props.removeCharacter(index)}>Delete</button></td>  
             </tr>  
            
-           
+          
             )
          })}
+{/* </Carousel>  */}
            </Flickity>
+         //   </div>
         );
     }
     //);
