8d84ecced6b083ad6571237e648226b23b132ab0
kirillboravik69 <kirillboravik69@gmail.com>
2023-12-27 16:35:08 +0300

    pepe

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -1,20 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../components/flickity.css';
 import Flickity from 'react-flickity-component'
-import { useEffect } from 'react';
-import { Carousel } from 'react-responsive-carousel';
+
 
 const TableBody = props => {
   
-   // const rows = props.characterData.map((row, index) => {
         return (
-         // <div>
             <Flickity
             className='slider'
-            elementType={'tr'}
-            
+            elementType={'tr'}  
          >
-             {/* <Carousel className='crsl'> */}
          {props.characterData.map((row, index) => {
             return ( 
                
@@ -26,16 +21,9 @@ const TableBody = props => {
                 <td><button onClick={() => props.removeCharacter(index)}>Delete</button></td>  
             </tr>  
            
-          
             )
          })}
-{/* </Carousel>  */}
-           </Flickity>
-         //   </div>
+           </Flickity>  
         );
     }
-    //);
-
-   // return <tbody>{rows}</tbody>;
-//}
 export default TableBody
